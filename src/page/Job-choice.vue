725c0949fd3391f6d7ed1367033a9ba3d0476a53
pair <huphup_1@126.com>
2019-03-22 18:01:20 +0800

    下班存档，开始有想法要怎么写了

--- src/page/Job-choice.vue
+++ src/page/Job-choice.vue
@@ -1,20 +1,23 @@
 <template>
     <div class="cui-content">
-        <div :class="['choice-content']">
+        <div class="choice-content">
             <!-- 大范围职业 -->
-            <div :class="['choice','choice-part']">
-                <!-- <input v-model="" type="text"> -->
+            <div class="choice choice-part">
                 <ul>
-                    <li v-for="item in choicePart" :key="item.id">{{item}}</li>
+                    <li><button class="choice-button" @click="Part(item.code)" v-for="item in choicePart" :key="item.id">{{item}}</button></li>
                 </ul>
             </div>
             <!-- 小范围职业 -->
-            <div :class="['choice','choice-major']">
-                <input v-model="choiceMajor" type="text">
+            <div class="choice choice-major">
+                <ul>
+                    <li><button class="choice-button" v-for="item in choiceMajor" :key="item.id">{{item}}</button></li>
+                </ul>
             </div>
             <!-- 具体职业 -->
-            <div :class="['choice','choice-concrete']">
-                <input v-model="choiceConcrete" type="text">
+            <div class="choice choice-concrete">
+                <ul>
+                    <li><button class="choice-button" v-for="item in choiceConcrete" :key="item.id">{{item}}</button></li>
+                </ul>
             </div>    
         </div>
     </div>
@@ -24,14 +27,29 @@ export default {
     name:'job-choice',
     data(){
         return {
-            choicePart:[12,23],
-            choiceMajor:[],
-            choiceConcrete:[]
+            choicePart:[
+                {code:1,name:'产品系列'},
+                {code:2,name:'金融系列'}
+            ],
+            choiceMajor:[
+                {parCode:1,name:'产品经理',childcode:1001},
+                {parCode:2,name:'部门经理',childcode:2001},
+            ],
+            choiceConcrete:[
+                {parCode:1001,name:'部门副经理'},
+                {parCode:2001,name:'部门经理'},
+            ],
+            
+            partNum:0,
+            majorNum:0,
+            concrete:0
         }
     },
     methods:
     {
-        
+        // Part(num){
+        //     partNum=this.num;
+        // }
     },
     created()
     {
@@ -66,5 +84,17 @@ export default {
     .choice-concrete
     {
         background-color:#EDEDED
+    }
+    .choice-button{
+        margin:0;
+        padding:0;
+        background-color: rgba(255, 255, 255, 0);
+        width: 100%;
+        border: none;
+        cursor: pointer;
+        
+    }
+    .button-active{
+        color:#74c7ff;
     }
 </style>
